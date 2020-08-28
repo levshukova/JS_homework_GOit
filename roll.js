b314c2e60951ad5abf761090bc000dcc2e94f7d9
levshukova <olevshukova@gmail.com>
2020-08-28 15:02:17 +0300

    New assignments added

---
+++ roll.js
@@ -0,0 +1,17 @@
+function roll(min, max, floatFlag) {
+  let r = Math.random() * (max - min) + min;
+  return floatFlag ? r : Math.floor(r);
+}
+
+let userNames = ["James", "Jane", "Ryan", "Rebecca"];
+
+// Ages between 12 and 64
+
+// Heights between 5.1 and 6 meters
+
+let user = {
+  name: userNames[roll(0, userNames.length)],
+  age: roll(12, 65),
+  height: roll(5.1, 6.1, 1).toFixed(1),
+};
+console.log(user);
